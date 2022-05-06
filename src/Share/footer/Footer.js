6bafc6e805bfa2ad64c51be48b3aa0a0a65a29b0
firoj7github <fkfiroj02@gmail.com>
2022-05-06 06:59:37 -0700

    third-commit

--- src/Share/footer/Footer.js
+++ src/Share/footer/Footer.js
@@ -5,6 +5,23 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer-all'>
+         <div className='footer-1'>
+             <h3>Help & Support</h3>
+             <p>Contact Us</p>
+             <p>Contact Us</p>
+             <p>Contact Us</p>
+             <p>Contact Us</p>
+             
+             </div> 
+         <div className='footer-2'>
+             <h3>Our Location</h3>
+             <p>Dhaka Office</p>
+             <p>Dhaka Office</p>
+             <p>Dhaka Office</p>
+             <p>Dhaka Office</p>
+             </div> 
+         <div className='footer-3'>
+             <h3>Follow Us</h3></div> 
           
         </div>
     );
